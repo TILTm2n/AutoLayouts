65b493e71877d2c70bb60a1cdc55c4c6249d6e05
Eugene <evgen29031999@bk.ru>
2022-01-31 15:08:12 +0300

    add locationLabel

--- AutoLayoutsApp/Controllers/LocationViewController.swift
+++ AutoLayoutsApp/Controllers/LocationViewController.swift
@@ -9,21 +9,55 @@ import UIKit
 
 class LocationViewController: UIViewController {
 
+    let locationLabel = UILabel()
+    let dateLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.view.backgroundColor = UIColor(named: "mainBGLight")
+        view.addSubview(locationLabel)
+        view.addSubview(dateLabel)
+        
+        setLocationLabel()
+        setLocationLabelConstraints()
+        
+        setDateLabel()
+        setDateLabelConstraints()
+    }
+
+    func setLocationLabel(){
+        locationLabel.text = "Simpheropol Crimea"
+        //locationLabel.font = UIFont(name: "RobotoSlab-Medium", size: 30)
+        locationLabel.font = UIFont.boldSystemFont(ofSize: 30.0)
+        locationLabel.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        locationLabel.layer.shadowOffset = CGSize(width: 5, height: 5)
+        locationLabel.layer.shadowOpacity = 0.5
+        locationLabel.textAlignment = .center
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setLocationLabelConstraints(){
+        locationLabel.translatesAutoresizingMaskIntoConstraints = false
+        locationLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 45.0).isActive = true
+        //locationLabel.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
+        locationLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
     }
-    */
+    
+    func setDateLabel(){
+        dateLabel.text = "Jan 30,2022"
+        dateLabel.textAlignment = .right
+        dateLabel.textColor = .white
+        dateLabel.font = UIFont(name: "RobotoSlab-Light", size: 15)
+        
+    }
+    
+    func setDateLabelConstraints(){
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        dateLabel.topAnchor.constraint(equalTo: locationLabel.bottomAnchor, constant: 5).isActive = true
+        dateLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        
+    }
 
 }
